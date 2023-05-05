8f204616245216d28ae2b5f53bc0ac01a4788e80
Guilherme Tavares <guitavarescar@gmail.com>
2023-05-05 13:53:12 -0300

    Aula 2

---
+++ src/01-intro/aula02.py
@@ -0,0 +1,23 @@
+# Palavras reservadas: Não podem ser usadas para nomear variáveis, constantes etc.
+# Exemplos: True, False, None, and, import, lambda.
+
+# Indentificadores são nomes de variáveis, classes ou funções
+# Python é case-sensitive
+# Nomes de variáveis devem começar com letras ou _
+# Sem espaços em branco ou caracteres especiais
+
+nome = "Maria"
+idade = 30
+
+# A convenção para nomeação de variáveis é a snake_case
+
+nome_completo = "Maria dos Anjos"
+
+# Constantes devem ser nomeadas em letras maiúsculas
+
+PI = 3.14159265
+
+# Nomear de forma clara, segundo a utilidade da variável
+
+c = 10 + 20 # ERRADO
+contador = 10 + 20 # CERTO
