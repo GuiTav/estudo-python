f241d8e97e12500d179a70cb8a69ace2bac9cf7c
Guilherme Tavares <guitavarescar@gmail.com>
2023-05-05 13:58:52 -0300

    Aula 3 - Comentários

---
+++ src/01-intro/aula03.py
@@ -0,0 +1,20 @@
+
+# Comentários de uma linha são feitos com hashtag
+
+"""
+Comentários
+de
+várias
+linhas
+são
+feitos
+com
+três
+aspas
+"""
+
+# Ctrl + ; para comentar/descomentar seções ou linhas de código
+
+n1 = 1
+# n2 = 3
+n3 = 5
